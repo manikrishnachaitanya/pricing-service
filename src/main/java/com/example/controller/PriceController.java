0f66c65dbde3ed9e3bc704398d99e89dd46607ec
manikrishnachaitanya <usha@ushas-mbp.lan>
2025-03-21 22:42:56 -0400

    Added the price estimator service

--- src/main/java/com/example/controller/PriceController.java
+++ src/main/java/com/example/controller/PriceController.java
@@ -1,13 +1,15 @@
 package com.example.controller;
 
+import com.example.model.Car;
 import com.example.service.PriceEstimationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/price-estimate")
+@RequestMapping("/price")
 public class PriceController {
 
     @Autowired
@@ -19,5 +21,9 @@ public class PriceController {
     }
 
     // TODO Add Apis
+    @GetMapping("/estimate")
+    public int estimate(@RequestBody Car car) {
+        return priceEstimationService.getPriceEstimate(car);
+    }
 
 }
